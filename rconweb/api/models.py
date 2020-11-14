70aa4cd00affe8ae992b127bdf0bead9c0e885b3
ProfessorZ <professor@profszone.xyz>
2020-11-14 10:31:15 +0000

    adding permission to model

--- rconweb/api/models.py
+++ rconweb/api/models.py
@@ -6,3 +6,7 @@ class SteamPlayer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     steam_id_64 = models.CharField(max_length=100)
 
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    class Meta:
+        permissions = [('admin_rights',"full permissions")]
